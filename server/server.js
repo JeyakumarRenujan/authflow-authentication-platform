9af6a6bec6f41b6ff947f40164d4944d91780ea9
JeyakumarRenujan <renujanrenu5@gmail.com>
2026-07-07 15:50:25 +0530

    setup mongodb

---
+++ server/server.js
@@ -0,0 +1,43 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+import connectDB from "./config/database.js";
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.use(helmet());
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}));
+
+app.use(express.json());
+
+app.use(express.urlencoded({
+    extended: true,
+}));
+
+app.use(cookieParser());
+
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+    res.json({
+        success: true,
+        message: "AuthFlow API is running..."
+    });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
